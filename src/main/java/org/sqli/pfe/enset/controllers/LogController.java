d92bad224d9de6cc1eb6ff8f007c3040422e23ad
mdouhi <mdouhi@sqli.com>
2022-07-01 15:46:53 +0100

    [Task] Delete TestController

--- src/main/java/org/sqli/pfe/enset/controllers/LogController.java
+++ src/main/java/org/sqli/pfe/enset/controllers/LogController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/logs")
+@RequestMapping({"/", "/logs"})
 public class LogController {
 
     private static final String LOGS_TEMPLATES = "/logs";
