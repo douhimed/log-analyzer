f0b429dceb7e8f0606f1723fb09079cc65e82e4d
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2022-07-20 11:50:51 +0100

    [LA-18]Feedbacks

--- src/main/java/org/sqli/pfe/enset/batch/writer/LogWriter.java
+++ src/main/java/org/sqli/pfe/enset/batch/writer/LogWriter.java
@@ -5,9 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.sqli.pfe.enset.models.entities.LogEntity;
 import org.sqli.pfe.enset.repositories.LogRepository;
+import org.sqli.pfe.enset.utils.constants.CommonConstants;
 import org.sqli.pfe.enset.utils.enums.LogMethodEnum;
 import org.sqli.pfe.enset.utils.enums.LogPathEnum;
-import org.sqli.pfe.enset.utils.json.JsonUtils;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,9 +21,14 @@ public class LogWriter implements ItemWriter<LogEntity> {
     @Override
     public void write(List<? extends LogEntity> logs) throws Exception {
         List<? extends LogEntity> logsFiltred = logs.stream()
-                .filter(logEntity -> LogMethodEnum.isELigibleMethod(
-                        JsonUtils.getNodeValueAtPath(logEntity.getBody(), LogPathEnum.METHOD.getValue())))
+                .filter(this::isLogEligibleToBePersisted)
                 .collect(Collectors.toList());
         this.logRepository.saveAll(logsFiltred);
     }
+
+    private boolean isLogEligibleToBePersisted(LogEntity logEntity) {
+        return  CommonConstants.RESPONSE.equalsIgnoreCase(logEntity.getValueAtPath(LogPathEnum.TYPE))
+                || LogMethodEnum.isELigibleMethod(logEntity.getValueAtPath(LogPathEnum.METHOD));
+    }
+
 }
--- src/main/java/org/sqli/pfe/enset/utils/constants/CommonConstants.java
+++ src/main/java/org/sqli/pfe/enset/utils/constants/CommonConstants.java
@@ -3,4 +3,6 @@ package org.sqli.pfe.enset.utils.constants;
 public class CommonConstants {
 
     public static final String EMPTY_STRING = "";
+
+    public static final String RESPONSE = "response";
 }
