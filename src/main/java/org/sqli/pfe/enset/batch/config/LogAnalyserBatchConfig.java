4fd14c104c79d1337c7b99bf5b54f520b879c219
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2022-07-28 17:18:18 +0100

    Delete some importation

--- src/main/java/org/sqli/pfe/enset/batch/config/LogAnalyserBatchConfig.java
+++ src/main/java/org/sqli/pfe/enset/batch/config/LogAnalyserBatchConfig.java
@@ -18,7 +18,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.Resource;
 import org.sqli.pfe.enset.models.entities.LogEntity;
 import org.sqli.pfe.enset.utils.dtos.LogDto;
 
